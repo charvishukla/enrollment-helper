52fb7ee83fe4037e0752a42b37391879dc758141
Charvi Shukla <67646308+charvishukla@users.noreply.github.com>
2022-12-24 12:58:00 +0530

    initialized react app

--- old/major_db_to_json.js
+++
@@ -1,5 +0,0 @@
-/**
- * Write a javascript function that accepts the major's code as a parameter 
- * and fetches a JSON object representing all the data about this major inside the 
- * data base  
- */
--- old/update_catalog.js
+++
@@ -1,6 +0,0 @@
-/**
- * (cron)
- * - Iterates thru the list of all majors and updates the db.
- * - Each major has a 10 second delay.
- */
-
